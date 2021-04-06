313bae9062ca24e53e7c2227afc82e7f7434d9b6
Fredrik Olsson <freol@outlook.com>
2021-04-06 14:06:07 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -6,25 +6,26 @@ import codecs
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'jasmine'
-DESCRIPTION = 'NMEA 0183 and 2000 parser'
+NAME = "jasmine"
+DESCRIPTION = "NMEA 0183 and 2000 parser"
 
-URL = 'https://github.com/rise-mo/jasmine'
-EMAIL = 'fredrik.x.olsson@ri.se'
-AUTHOR = 'Rise Maritime Operations'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.0'
+URL = "https://github.com/rise-mo/jasmine"
+EMAIL = "fredrik.x.olsson@ri.se"
+AUTHOR = "RISE Research Institute of Sweden"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.1.0"
 
 # Long description
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
+
 
 LONG_DESCRIPTION = read("README.md")
 
 # What packages are required for this module to be executed?
 # Parse the requirements-txt file and use for install_requires in pip
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     REQUIRED = f.read().splitlines()
 
 # What packages are optional?
@@ -35,9 +36,9 @@ EXTRAS = {
 # Trove classifiers (i.e. metadata) for package discovery.
 # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
 TROVE_CLASSIFIERS = [
-  'Programming Language :: Python',
-  'Programming Language :: Python :: 3',
-  'Programming Language :: Python :: 3.7',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
 ]
 
 # Here be dragons
@@ -48,7 +49,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     python_requires=REQUIRES_PYTHON,
     url=URL,
@@ -56,6 +57,6 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=TROVE_CLASSIFIERS,
 )
